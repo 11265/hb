7f4d8778d0212dcbcccadc1435a1651441be942c
11265 <xdllll@163.com>
2024-07-28 16:57:46 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -3,39 +3,40 @@
 .p2align 2
 
 _main:
-    // 保存调用者保存的寄存器
     stp x19, x20, [sp, #-16]!
     stp x29, x30, [sp, #-16]!
     mov x29, sp
 
-    // 打印欢迎消息
     adrp x0, message@PAGE
     add x0, x0, message@PAGEOFF
     bl _printf
 
-    // 获取当前进程 ID
     bl _getpid
-    mov x19, x0  // 保存当前进程 ID
+    mov x19, x0
 
-    // 打印当前进程 ID
     adrp x0, current_pid_format@PAGE
     add x0, x0, current_pid_format@PAGEOFF
     mov x1, x19
     bl _printf
 
-    // 通过名称获取指定进程 ID
+    adrp x0, log_before_get_pid@PAGE
+    add x0, x0, log_before_get_pid@PAGEOFF
+    bl _printf
+
     adrp x0, process_name@PAGE
     add x0, x0, process_name@PAGEOFF
     bl _get_pid_by_name
 
-    // 检查返回值
+    mov x1, x0
+    adrp x0, log_after_get_pid@PAGE
+    add x0, x0, log_after_get_pid@PAGEOFF
+    bl _printf
+
     cmp x0, #0
     ble _not_found
 
-    // 保存找到的 PID
     mov x19, x0
 
-    // 打印找到的进程 ID
     adrp x0, found_pid_format@PAGE
     add x0, x0, found_pid_format@PAGEOFF
     mov x1, x19
@@ -44,13 +45,11 @@ _main:
     b _exit_program
 
 _not_found:
-    // 打印未找到进程的消息
     adrp x0, not_found_message@PAGE
     add x0, x0, not_found_message@PAGEOFF
     bl _printf
 
 _exit_program:
-    // 恢复寄存器并返回
     ldp x29, x30, [sp], #16
     ldp x19, x20, [sp], #16
     mov w0, #0
@@ -61,9 +60,13 @@ message:
     .asciz "iOS Assembly!\n"
 current_pid_format:
     .asciz "Current PID: %d\n"
+log_before_get_pid:
+    .asciz "Before calling get_pid_by_name\n"
+log_after_get_pid:
+    .asciz "After calling get_pid_by_name, result: %d\n"
 found_pid_format:
     .asciz "Found process PID: %d\n"
 not_found_message:
     .asciz "Process not found or error occurred\n"
 process_name:
-    .asciz "SpringBoard"
+    .asciz "main"  // Change this to the name of your program
