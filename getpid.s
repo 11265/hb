bff34e8937a6d10739e802279296c46085cd8a23
11265 <xdllll@163.com>
2024-07-28 11:26:46 +0800

    更新 getpid.s

--- getpid.s
+++ getpid.s
@@ -1,6 +1,9 @@
 .section __TEXT,__text
 .globl _main
+.globl _get_pid
 .p2align 2
+
+// 主程序入口
 _main:
     // 打印消息
     mov x0, #1                     // 文件描述符: stdout
@@ -10,14 +13,21 @@ _main:
     mov x16, #4                    // syscall: write
     svc #0x80                      // 进行系统调用
 
-    // 获取进程 ID
-    mov x16, #20                   // syscall: getpid
-    svc #0x80                      // 进行系统调用
+    // 调用获取进程 ID 的函数
+    bl _get_pid                    // 调用 _get_pid 函数
+
+    // 获取的进程 ID 现在在 x0 中，可以在此处进行其他操作
 
     // 退出程序
     mov x16, #1                    // syscall: exit
     mov x0, #0                     // 退出码
     svc #0x80                      // 进行系统调用
+
+// 获取进程 ID 的函数
+_get_pid:
+    mov x16, #20                   // syscall: getpid
+    svc #0x80                      // 进行系统调用
+    ret                            // 返回，进程 ID 保存在 x0 中
 
 .section __DATA,__data
 message:
