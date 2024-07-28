99dd6fa4664708c261a2dd81a09f4d88adbe5777
11265 <xdllll@163.com>
2024-07-28 23:23:03 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -31,11 +31,58 @@ _main:
     cmp x0, #0
     b.le _error_read
 
-    // 打印读取成功消息
-    adrp x0, read_success_msg@PAGE
-    add x0, x0, read_success_msg@PAGEOFF
+    // 执行ps命令
+    adrp x0, ps_command@PAGE
+    add x0, x0, ps_command@PAGEOFF
+    adrp x1, read_mode@PAGE
+    add x1, x1, read_mode@PAGEOFF
+    bl _popen
+
+    // 检查popen是否成功
+    cmp x0, #0
+    b.eq _error_popen
+
+    // 保存文件指针
+    mov x19, x0
+
+    // 打印ps输出
+    adrp x0, ps_output_msg@PAGE
+    add x0, x0, ps_output_msg@PAGEOFF
     bl _puts
 
+    // 为读取缓冲区分配栈空间
+    sub sp, sp, #1024
+    mov x21, sp
+
+_read_loop:
+    // 读取一行
+    mov x0, x21
+    mov x1, #1024
+    mov x2, x19
+    bl _fgets
+
+    // 检查是否读到EOF
+    cmp x0, #0
+    b.eq _end_read
+
+    // 打印读取的行
+    mov x0, x21
+    bl _printf
+
+    b _read_loop
+
+_end_read:
+    // 关闭文件
+    mov x0, x19
+    bl _pclose
+
+    b _exit
+
+_error_popen:
+    // 打印popen错误消息
+    adrp x0, error_popen_msg@PAGE
+    add x0, x0, error_popen_msg@PAGEOFF
+    bl _puts
     b _exit
 
 _error_read:
@@ -58,10 +105,16 @@ _exit:
 start_msg:
     .asciz "程序开始执行"
 input_prompt:
-    .asciz "请输入内容: "
-read_success_msg:
-    .asciz "成功读取输入"
+    .asciz "请输入要查找的进程名称: "
+ps_command:
+    .asciz "ps -e"
+read_mode:
+    .asciz "r"
+ps_output_msg:
+    .asciz "ps命令输出："
+error_popen_msg:
+    .asciz "错误：无法执行ps命令"
 error_read_msg:
-    .asciz "读取输入时发生错误"
+    .asciz "错误：读取输入失败"
 end_msg:
     .asciz "程序执行结束"
