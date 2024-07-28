6c8ff730dfb1b05b31dabca5c17cb6c0beeb1af3
11265 <xdllll@163.com>
2024-07-28 23:48:58 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -1,52 +1,49 @@
-.section __TEXT,__text
-.globl _main
+.global _main
 .align 2
 
 _main:
-    stp x29, x30, [sp, #-16]!
-    mov x29, sp
+    // 保存链接寄存器
+    stp     x29, x30, [sp, #-16]!
+    mov     x29, sp
 
-    // 打印开始消息
-    adrp x0, start_msg@PAGE
-    add x0, x0, start_msg@PAGEOFF
-    bl _puts
+    // 打印开始信息
+    adrp    x0, start_msg@PAGE
+    add     x0, x0, start_msg@PAGEOFF
+    bl      _printf
 
-    // 调用C函数获取PVZ的PID
-    bl _get_pvz_pid
+    // 调用 get_pvz_pid 函数
+    bl      _get_pvz_pid
 
-    // 检查返回值
-    cmp x0, #-1
-    b.eq _not_found
+    // 检查返回值是否为 -1
+    cmp     w0, #-1
+    b.eq    not_found
 
-    // 打印结果（如果找到）
-    mov x1, x0
-    adrp x0, found_proc_msg@PAGE
-    add x0, x0, found_proc_msg@PAGEOFF
-    bl _printf
-    b _exit
+    // 打印找到进程的信息
+    mov     w1, w0  // 将返回的 PID 移动到 w1 作为第二个参数
+    adrp    x0, found_msg@PAGE
+    add     x0, x0, found_msg@PAGEOFF
+    bl      _printf
+    b       end
 
-_not_found:
-    // 打印未找到进程的消息
-    adrp x0, not_found_msg@PAGE
-    add x0, x0, not_found_msg@PAGEOFF
-    bl _puts
+not_found:
+    // 打印未找到进程的信息
+    adrp    x0, not_found_msg@PAGE
+    add     x0, x0, not_found_msg@PAGEOFF
+    bl      _printf
 
-_exit:
-    // 打印结束消息
-    adrp x0, end_msg@PAGE
-    add x0, x0, end_msg@PAGEOFF
-    bl _puts
+end:
+    // 打印结束信息
+    adrp    x0, end_msg@PAGE
+    add     x0, x0, end_msg@PAGEOFF
+    bl      _printf
 
-    mov x0, #0
-    mov x16, #1 // exit系统调用
-    svc #0x80
+    // 恢复链接寄存器并返回
+    ldp     x29, x30, [sp], #16
+    mov     w0, #0
+    ret
 
-.section __TEXT,__cstring
-start_msg:
-    .asciz "程序开始执行，正在查找PVZ进程..."
-found_proc_msg:
-    .asciz "找到PVZ进程，PID: %d\n"
-not_found_msg:
-    .asciz "未找到PVZ进程"
-end_msg:
-    .asciz "程序执行结束"
+.data
+start_msg:      .asciz "程序开始执行，正在查找PVZ进程...\n"
+found_msg:      .asciz "找到PVZ进程，PID: %d\n"
+not_found_msg:  .asciz "未找到PVZ进程\n"
+end_msg:        .asciz "程序执行结束\n"
