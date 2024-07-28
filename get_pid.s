4e47cd5898462098dcea0e589887df166d0b0256
11265 <xdllll@163.com>
2024-07-28 13:28:13 +0800

    更新 get_pid.s

--- get_pid.s
+++ get_pid.s
@@ -10,6 +10,11 @@ _get_pid_by_name:
 
     mov x19, x0  // 保存进程名称指针
 
+    // 打印调试信息
+    adrp x0, debug_start@PAGE
+    add x0, x0, debug_start@PAGEOFF
+    bl _printf
+
     // 分配内存用于存储 PID 列表
     mov x0, #4096  // 4KB 应该足够存储大多数情况下的 PID 列表
     bl _malloc
@@ -17,6 +22,12 @@ _get_pid_by_name:
 
     // 检查内存分配是否成功
     cbz x20, _allocation_failed
+
+    // 打印调试信息
+    adrp x0, debug_malloc@PAGE
+    add x0, x0, debug_malloc@PAGEOFF
+    mov x1, x20
+    bl _printf
 
     // 调用 proc_listpids 获取所有 PID
     mov x0, #1  // PROC_ALL_PIDS
@@ -31,6 +42,12 @@ _get_pid_by_name:
 
     mov x21, x0  // 保存返回的字节数
     mov x22, #0  // PID 计数器
+
+    // 打印调试信息
+    adrp x0, debug_listpids@PAGE
+    add x0, x0, debug_listpids@PAGEOFF
+    mov x1, x21
+    bl _printf
 
 _pid_loop:
     ldr w0, [x20, x22, lsl #2]  // 加载 PID
@@ -61,13 +78,29 @@ _pid_loop:
 _found_pid:
     ldr w0, [x20, x22, lsl #2]  // 返回找到的 PID
 
+    // 打印调试信息
+    adrp x1, debug_found@PAGE
+    add x1, x1, debug_found@PAGEOFF
+    bl _printf
+
 _cleanup:
     mov x1, x20
     bl _free
+
+    // 打印调试信息
+    adrp x0, debug_end@PAGE
+    add x0, x0, debug_end@PAGEOFF
+    bl _printf
+
     b _exit
 
 _allocation_failed:
     mov x0, #-1  // 返回错误码
+
+    // 打印调试信息
+    adrp x0, debug_alloc_failed@PAGE
+    add x0, x0, debug_alloc_failed@PAGEOFF
+    bl _printf
 
 _exit:
     ldp x29, x30, [sp], #16
@@ -76,3 +109,15 @@ _exit:
     ret
 
 .section __DATA,__data
+debug_start:
+    .asciz "Debug: Starting get_pid_by_name\n"
+debug_malloc:
+    .asciz "Debug: Malloc returned %p\n"
+debug_listpids:
+    .asciz "Debug: proc_listpids returned %d bytes\n"
+debug_found:
+    .asciz "Debug: Found PID %d\n"
+debug_end:
+    .asciz "Debug: Ending get_pid_by_name\n"
+debug_alloc_failed:
+    .asciz "Debug: Memory allocation failed\n"
