604bf9cc38a2d2e2d2b02418358120d027e467e4
11265 <xdllll@163.com>
2024-07-28 11:37:38 +0800

    更新 getpid.s

--- getpid.s
+++ getpid.s
@@ -58,15 +58,15 @@ _print_digit:
     udiv x5, x1, x7                // 计算 x1 / 10
     msub x6, x1, x5, x7            // 计算余数 x1 % 10
     add x6, x6, x4                 // 转换为字符
-    strb w6, [x2, x3]              // 存储字符
+    sub x2, x2, #1                 // 移动指针到缓冲区前一个位置
+    strb w6, [x2]                  // 存储字符
     mov x1, x5                     // 更新 x1 为商
     add x3, x3, #1                 // 增加字符数
     cmp x1, #0                     // 检查是否结束
     bne _print_digit               // 如果不是 0，继续
 
-    // 反向输出字符串
-    sub x2, x2, x3                 // 指向缓冲区起始位置
-    add x0, x2, #0                 // 设置输出字符的指针
+    mov x0, #1                     // 文件描述符: stdout
+    mov x1, x2                     // 设置输出字符的指针
     mov x2, x3                     // 设置输出字符的长度
     mov x16, #4                    // syscall: write
     svc #0x80                      // 输出字符
