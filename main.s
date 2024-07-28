56c957f21cd86ce3e8890bdd0592f5e255eb9a56
11265 <xdllll@163.com>
2024-07-28 15:18:19 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -3,47 +3,35 @@
 .p2align 2
 
 _main:
-    // 打印消息
-    mov x0, #1                     // 文件描述符：stdout
-    adrp x1, message@PAGE          // 获取 message 的页地址
-    add x1, x1, message@PAGEOFF    // 添加页内偏移
-    mov x2, #21                    // 消息长度 (包括换行符)
-    mov x16, #4                    // 系统调用号：write
-    svc #0x80                      // 进行系统调用
+    // 保存调用者保存的寄存器
+    stp x19, x20, [sp, #-16]!
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
 
-    // 调用获取进程 ID 的函数
-    bl _get_pid
+    // 打印欢迎消息
+    adrp x0, message@PAGE
+    add x0, x0, message@PAGEOFF
+    bl _printf
 
-    // 保存 PID
-    mov x19, x0                    // 将 PID 保存到 x19 中
+    // 获取当前进程 ID
+    bl _getpid
+    mov x19, x0  // 保存当前进程 ID
 
-    // 打印 "PID: " 消息
-    mov x0, #1                     // 文件描述符：stdout
-    adrp x1, pid_message@PAGE      // 获取 pid_message 的页地址
-    add x1, x1, pid_message@PAGEOFF// 添加页内偏移
-    mov x2, #5                     // 消息长度
-    mov x16, #4                    // 系统调用号：write
-    svc #0x80                      // 进行系统调用
+    // 打印当前进程 ID
+    adrp x0, current_pid_format@PAGE
+    add x0, x0, current_pid_format@PAGEOFF
+    mov w1, w19  // 确保 PID 作为 32 位整数传递
+    bl _printf
 
-    // 打印实际的 PID 数字
-    mov x0, x19                    // 将 PID 传递给打印函数
-    bl _print_number               // 调用 _print_number 函数
-
-    // 打印换行符
-    mov x0, #1                     // 文件描述符：stdout
-    adrp x1, newline@PAGE          // 获取 newline 的页地址
-    add x1, x1, newline@PAGEOFF    // 添加页内偏移
-    mov x2, #1                     // 消息长度
-    mov x16, #4                    // 系统调用号：write
-    svc #0x80                      // 进行系统调用
-
-    // 退出程序
-    mov x16, #1                    // 系统调用号：exit
-    mov x0, #0                     // 退出码
-    svc #0x80                      // 进行系统调用
+_exit_program:
+    // 恢复寄存器并返回
+    ldp x29, x30, [sp], #16
+    ldp x19, x20, [sp], #16
+    mov w0, #0
+    ret
 
 .section __DATA,__data
-pid_message:
-    .asciz "PID: "                 // PID 前缀消息
-newline:
-    .asciz "\n"                    // 换行符
+message:
+    .asciz "iOS Assembly: Get Current PID\n"
+current_pid_format:
+    .asciz "Current PID: %d\n"
