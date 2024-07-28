3060c3727e159ec90d08386548bc74b2b28d798f
11265 <xdllll@163.com>
2024-07-28 13:22:20 +0800

    更新 api.h

--- api.h
+++ api.h
@@ -1,8 +1,12 @@
 #ifndef API_H
 #define API_H
 
-extern void _get_pid(void);
-extern void _print_number(void);
-extern int _get_pid_by_name(const char* name);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <libproc.h>
+
+extern int get_pid_by_name(const char* name);
 
 #endif // API_H
