a8359e97ede06c65b1156472316a6042843b0850
11265 <xdllll@163.com>
2024-07-28 22:17:06 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -30,57 +30,34 @@ _main:
     mov x16, #202  // sysctl 系统调用号
     svc #0x80
 
+    // 保存返回值
+    mov x19, x0
+
+    // 打印返回值
+    adrp x0, sysctl_return_msg@PAGE
+    add x0, x0, sysctl_return_msg@PAGEOFF
+    mov x1, x19
+    bl _printf
+
     // 检查返回值
-    cmp x0, #0
-    b.ne _error
+    cmp x19, #0
+    b.eq _success
 
-    // 遍历进程列表
-    adrp x19, buffer@PAGE
-    add x19, x19, buffer@PAGEOFF
-    adrp x20, buffer_size@PAGE
-    add x20, x20, buffer_size@PAGEOFF
-    ldr x20, [x20]
-    add x20, x19, x20  // 结束地址
+    // 如果失败，获取 errno
+    bl ___error
+    ldr w1, [x0]
 
-_loop:
-    cmp x19, x20
-    b.ge _not_found
-
-    // 获取进程名称
-    add x0, x19, #0x1bc  // proc_name 在 kinfo_proc 结构中的偏移
-    adrp x1, process_name@PAGE
-    add x1, x1, process_name@PAGEOFF
-    bl _strcmp
-
-    // 如果名称匹配，提取 PID
-    cmp x0, #0
-    b.eq _found
-
-    // 移动到下一个 kinfo_proc
-    add x19, x19, #0x260  // kinfo_proc 结构的大小
-    b _loop
-
-_found:
-    // 提取 PID
-    ldr w1, [x19, #0x68]  // PID 在 kinfo_proc 结构中的偏移
-
-    // 打印 PID
-    adrp x0, found_msg@PAGE
-    add x0, x0, found_msg@PAGEOFF
-    bl _printf
-    b _exit
-
-_not_found:
-    // 打印未找到消息
-    adrp x0, not_found_msg@PAGE
-    add x0, x0, not_found_msg@PAGEOFF
-    bl _printf
-    b _exit
-
-_error:
-    // 打印错误消息
+    // 打印 errno
     adrp x0, error_msg@PAGE
     add x0, x0, error_msg@PAGEOFF
+    bl _printf
+
+    b _exit
+
+_success:
+    // 打印成功消息
+    adrp x0, success_msg@PAGE
+    add x0, x0, success_msg@PAGEOFF
     bl _printf
 
 _exit:
@@ -89,24 +66,9 @@ _exit:
     svc #0x80
 
 .section __TEXT,__cstring
-found_msg:
-    .asciz "Process PID: %d\n"
-not_found_msg:
-    .asciz "Process not found\n"
+sysctl_return_msg:
+    .asciz "sysctl return value: %d\n"
 error_msg:
-    .asciz "sysctl failed\n"
-
-.section __TEXT,__text
-.globl _strcmp
-_strcmp:
-    ldrb w2, [x0], #1
-    ldrb w3, [x1], #1
-    cmp w2, w3
-    b.ne _strcmp_ne
-    cmp w2, #0
-    b.ne _strcmp
-    mov x0, #0
-    ret
-_strcmp_ne:
-    sub x0, x2, x3
-    ret
+    .asciz "sysctl failed with errno: %d\n"
+success_msg:
+    .asciz "sysctl call successful\n"
