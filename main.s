05b1cca1e25f9dc09343a7edb1954f09c26bb78f
11265 <xdllll@163.com>
2024-07-28 14:58:38 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -23,32 +23,6 @@ _main:
     mov x1, x19
     bl _printf
 
-    // 通过名称获取指定进程 ID
-    adrp x0, process_name@PAGE
-    add x0, x0, process_name@PAGEOFF
-    bl _get_pid_by_name
-
-    // 检查返回值
-    cmp x0, #0
-    ble _not_found
-
-    // 保存找到的 PID
-    mov x19, x0
-
-    // 打印找到的进程 ID
-    adrp x0, found_pid_format@PAGE
-    add x0, x0, found_pid_format@PAGEOFF
-    mov x1, x19
-    bl _printf
-
-    b _exit_program
-
-_not_found:
-    // 打印未找到进程的消息
-    adrp x0, not_found_message@PAGE
-    add x0, x0, not_found_message@PAGEOFF
-    bl _printf
-
 _exit_program:
     // 恢复寄存器并返回
     ldp x29, x30, [sp], #16
@@ -58,12 +32,6 @@ _exit_program:
 
 .section __DATA,__data
 message:
-    .asciz "iOS Assembly!\n"
+    .asciz "iOS Assembly: Get Current PID\n"
 current_pid_format:
     .asciz "Current PID: %d\n"
-found_pid_format:
-    .asciz "Found process PID: %d\n"
-not_found_message:
-    .asciz "Process not found or error occurred\n"
-process_name:
-    .asciz "pvz"
