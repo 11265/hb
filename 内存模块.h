d4242535bb24d200bf5923bf47357c08a4a9777e
11265 <xdllll@163.com>
2024-07-29 18:20:44 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -1,15 +1,11 @@
-#ifndef MEMORY_READ_H
-#define MEMORY_READ_H
+#ifndef MEMORY_MODULE_H
+#define MEMORY_MODULE_H
 
-#include <sys/types.h>
-#include <mach/mach_types.h>
+#include <mach/mach.h>
+#include <stdint.h>
 
 int initialize_memory_access(pid_t pid, vm_address_t address, vm_size_t size);
-void cleanup_memory_access();
-
+void cleanup_memory_access(void);
 int32_t 读内存i32(vm_address_t address);
-int64_t 读内存i64(vm_address_t address);
-float   读内存f32(vm_address_t address);
-double  读内存f64(vm_address_t address);
 
-#endif
+#endif // MEMORY_MODULE_H
