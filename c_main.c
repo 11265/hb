f29b0bf15f350dde21d7e1e55438b3d5608b9c12
11265 <xdllll@163.com>
2024-07-29 16:53:23 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -6,16 +6,6 @@
 #define TARGET_ADDRESS 0x102DD2404  // 替换为目标内存地址
 #define MAP_SIZE 4096  // 映射大小，可根据需要调整
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int c_main(void);
-
-#ifdef __cplusplus
-}
-#endif
-
 int c_main(void) {
     if (initialize_memory_access(TARGET_PID, TARGET_ADDRESS, MAP_SIZE) != 0) {
         return -1;
@@ -24,17 +14,11 @@ int c_main(void) {
     // 读取 int32_t
     int32_t int_value = 读内存i32(TARGET_ADDRESS);
     printf("读取的 int32_t 值: %d\n", int_value);
-/*
+
     // 读取 double
     double double_value = 读内存f64(TARGET_ADDRESS + sizeof(int32_t));
     printf("读取的 double 值: %f\n", double_value);
 
-    // 写入示例
-    写内存f64(TARGET_ADDRESS, 2.71828182845904);
-    printf("写入 double 数据: 2.71828182845904\n");
-    double_value = 读内存f64(TARGET_ADDRESS);
-    printf("读取 double 数据: %f\n", double_value);
-*/
     cleanup_memory_access();
     return 0;
 }
