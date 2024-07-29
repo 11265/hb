f03c62785fff6c5c1e81aaf8537baaad5c807c9c
11265 <xdllll@163.com>
2024-07-29 17:45:43 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 
 #define TARGET_PID 22496  // 替换为目标进程的 PID
-#define TARGET_ADDRESS 0x102DD2404  // 替换为目标内存地址
+#define TARGET_ADDRESS 0x100000000  //映射内存开始地址
+#define TARGET_ADDRESS2 0x102DD2404 //替换为目标内存地址
 
 int c_main(void) {
     if (initialize_memory_access(TARGET_PID, TARGET_ADDRESS, 4096) != 0) {
         fprintf(stderr, "无法初始化内存访问\n");
         return -1;
     }
-    
-    int32_t int_value = 读内存i32(TARGET_ADDRESS);
+
+    int32_t int_value = 读内存i32(TARGET_ADDRESS2);
     printf("读取的 int32_t 值: %d\n", int_value);
 
     cleanup_memory_access();
