980c4bb829acd004eda938de9c41627f6028fe3c
11265 <xdllll@163.com>
2024-07-29 12:31:46 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -5,10 +5,13 @@ _main:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
 
-    bl _read_process_memory
+    // 例如，读取 PID 为 1234 的进程中地址 0x10000000 处的 32 位整数
+    mov x0, #22496       // target_pid
+    mov x1, #0x10000000 // target_address
+    bl _read_int32
 
     ldp x29, x30, [sp], #16
     mov x0, #0
     ret
 
-.extern _read_process_memory
+.extern _read_int32
