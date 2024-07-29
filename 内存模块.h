e22e5594fe12675cfa72bab7a43f84ebb86ef233
11265 <xdllll@163.com>
2024-07-29 15:49:40 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -1,12 +1,14 @@
-#ifndef MEMORY_READ_H                                           // 防止头文件重复包含
+#ifndef MEMORY_READ_H
 #define MEMORY_READ_H
 
-#include <mach/mach.h>                                          // 包含Mach相关的头文件
-#include <sys/types.h>                                          // 包含系统类型定义，如pid_t
+#include <mach/mach.h>
+#include <sys/types.h>
 
-int initialize_task_port(pid_t pid);                            // 初始化任务端口的函数声明
-void cleanup_task_port();                                       // 清理任务端口的函数声明
-int 读内存i32(mach_vm_address_t address, int32_t *value);      // 读取32位整数的函数声明
-int 读内存i64(mach_vm_address_t address, int64_t *value);      // 读取64位整数的函数声明
+int initialize_task_port(pid_t pid);
+void cleanup_task_port();
+int32_t 读内存i32(mach_vm_address_t address);
+int64_t 读内存i64(mach_vm_address_t address);
+float 读内存f32(mach_vm_address_t address);
+double 读内存f64(mach_vm_address_t address);
 
-#endif                                                          // 结束头文件保护
+#endif
