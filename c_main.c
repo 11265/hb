c2abf16484672165c9b7008c8d2beb2548f52cb3
11265 <xdllll@163.com>
2024-07-29 16:02:34 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -22,6 +22,7 @@ int c_main() {
     int32_t int32_value = 读内存i32(TARGET_ADDRESS);
     printf("读取 int32_t 数据: %d\n", int32_value);
 
+/*
     int64_t int64_value = 读内存i64(TARGET_ADDRESS);
     printf("读取 int64_t 数据: %lld\n", int64_value);
 
@@ -36,23 +37,8 @@ int c_main() {
     printf("写入 int32_t 数据: 12345\n");
     int32_value = 读内存i32(TARGET_ADDRESS);
     printf("读取 int32_t 数据: %d\n", int32_value);
-
-    写内存i64(TARGET_ADDRESS, 9876543210LL);
-    printf("写入 int64_t 数据: 9876543210\n");
-    int64_value = 读内存i64(TARGET_ADDRESS);
-    printf("读取 int64_t 数据: %lld\n", int64_value);
-
-    写内存f32(TARGET_ADDRESS, 3.14159f);
-    printf("写入 float 数据: 3.14159\n");
-    float_value = 读内存f32(TARGET_ADDRESS);
-    printf("读取 float 数据: %f\n", float_value);
-
-    写内存f64(TARGET_ADDRESS, 2.71828182845904);
-    printf("写入 double 数据: 2.71828182845904\n");
-    double_value = 读内存f64(TARGET_ADDRESS);
-    printf("读取 double 数据: %f\n", double_value);
-
-    cleanup_task_port();
+*/
+    cleanup_task_port();//释放任务端口
 
     printf("c_main 执行完成\n");
     return 0;
