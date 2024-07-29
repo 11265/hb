f73979aeb888708b267eb3a945b21be8f89b6909
11265 <xdllll@163.com>
2024-07-29 16:53:21 +0800

    更新 内存模块.c

--- 内存模块.c
+++ 内存模块.c
@@ -1,36 +1,41 @@
 #include "内存模块.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <mach/mach_error.h>
-#include <stdbool.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 static void *mapped_memory = NULL;
 static size_t mapped_size = 0;
-static mach_vm_address_t base_address = 0;
+static off_t base_offset = 0;
 
 int initialize_memory_access(pid_t pid, mach_vm_address_t address, size_t size) {
     char mem_file[64];
     snprintf(mem_file, sizeof(mem_file), "/proc/%d/mem", pid);
     
-    int fd = open(mem_file, O_RDWR);
+    int fd = open(mem_file, O_RDONLY);
     if (fd == -1) {
-        perror("open failed");
+        fprintf(stderr, "open failed: %s\n", strerror(errno));
         return -1;
     }
 
-    mapped_memory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, address);
+    // 计算页面对齐的地址和大小
+    long page_size = sysconf(_SC_PAGESIZE);
+    off_t page_aligned_offset = (address / page_size) * page_size;
+    size_t page_aligned_size = size + (address - page_aligned_offset);
+
+    mapped_memory = mmap(NULL, page_aligned_size, PROT_READ, MAP_PRIVATE, fd, page_aligned_offset);
     if (mapped_memory == MAP_FAILED) {
-        perror("mmap failed");
+        fprintf(stderr, "mmap failed: %s\n", strerror(errno));
         close(fd);
         return -1;
     }
 
     close(fd);
-    mapped_size = size;
-    base_address = address;
+    mapped_size = page_aligned_size;
+    base_offset = address - page_aligned_offset;
     return 0;
 }
 
@@ -39,15 +44,15 @@ void cleanup_memory_access() {
         munmap(mapped_memory, mapped_size);
         mapped_memory = NULL;
         mapped_size = 0;
-        base_address = 0;
+        base_offset = 0;
     }
 }
 
 static void* get_mapped_address(mach_vm_address_t address) {
-    if (mapped_memory == NULL || address < base_address || address >= base_address + mapped_size) {
+    if (mapped_memory == NULL || address < base_offset || address >= base_offset + mapped_size) {
         return NULL;
     }
-    return (void*)((char*)mapped_memory + (address - base_address));
+    return (void*)((char*)mapped_memory + (address - base_offset));
 }
 
 // 读取函数
@@ -75,23 +80,4 @@ double 读内存f64(mach_vm_address_t address) {
     return *(double*)mapped_addr;
 }
 
-// 写入函数
-void 写内存i32(mach_vm_address_t address, int32_t value) {
-    void* mapped_addr = get_mapped_address(address);
-    if (mapped_addr != NULL) *(int32_t*)mapped_addr = value;
-}
-
-void 写内存i64(mach_vm_address_t address, int64_t value) {
-    void* mapped_addr = get_mapped_address(address);
-    if (mapped_addr != NULL) *(int64_t*)mapped_addr = value;
-}
-
-void 写内存f32(mach_vm_address_t address, float value) {
-    void* mapped_addr = get_mapped_address(address);
-    if (mapped_addr != NULL) *(float*)mapped_addr = value;
-}
-
-void 写内存f64(mach_vm_address_t address, double value) {
-    void* mapped_addr = get_mapped_address(address);
-    if (mapped_addr != NULL) *(double*)mapped_addr = value;
-}
+// 注意：移除了写入函数，因为mmap是以只读方式打开的
