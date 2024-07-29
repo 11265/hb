a70aeac130a057f78cde9a4fc01cf569fc2e239a
11265 <xdllll@163.com>
2024-07-29 12:31:58 +0800

    更新 main.s

--- main.s
+++ main.s
@@ -7,7 +7,7 @@ _main:
 
     // 例如，读取 PID 为 1234 的进程中地址 0x10000000 处的 32 位整数
     mov x0, #22496       // target_pid
-    mov x1, #0x10000000 // target_address
+    mov x1, #0x102DD2404 // target_address
     bl _read_int32
 
     ldp x29, x30, [sp], #16
