4e2b3bcb65ff7d8e14701aff995ccff74a8976ed
11265 <xdllll@163.com>
2024-07-29 17:22:46 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -1,11 +1,13 @@
 #include "内存模块.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TARGET_PID 22496  // 替换为目标进程的 PID
 #define TARGET_ADDRESS 0x102DD2404  // 替换为目标内存地址
 
 int c_main(void) {
     if (initialize_memory_access(TARGET_PID, TARGET_ADDRESS, 4096) != 0) {
+        fprintf(stderr, "无法初始化内存访问\n");
         return -1;
     }
 
