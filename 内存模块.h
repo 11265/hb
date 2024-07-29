339ba3a7b52a1883b809d379794c8a5f2ebd1800
11265 <xdllll@163.com>
2024-07-29 18:41:54 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -3,10 +3,30 @@
 
 #include <mach/mach.h>
 #include <stdint.h>
-#include <unistd.h> // 为 pid_t 添加这个
+#include <unistd.h>
+#include <pthread.h>
 
-int initialize_memory_access(pid_t pid, vm_address_t address, vm_size_t size);
-void cleanup_memory_access(void);
+#define MAX_CACHED_REGIONS 10
+#define REGION_SIZE 0x100000  // 1MB
+#define MAX_PENDING_REQUESTS 100
+
+typedef struct {
+    vm_address_t base_address;
+    void *mapped_memory;
+    vm_size_t mapped_size;
+} MemoryRegion;
+
+typedef struct {
+    vm_address_t address;
+    int32_t *result;
+    pthread_cond_t *cond;
+    pthread_mutex_t *mutex;
+    int *completed;
+} ReadRequest;
+
+int initialize_memory_module(pid_t pid);
+void cleanup_memory_module(void);
 int32_t 读内存i32(vm_address_t address);
+int32_t 异步读内存i32(vm_address_t address);
 
 #endif // MEMORY_MODULE_H
