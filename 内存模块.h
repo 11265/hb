382dd1fa480ec440f2e4a0c140b2d7c47dcc3934
11265 <xdllll@163.com>
2024-07-29 16:28:50 +0800

    实现内存映射

--- 内存模块.h
+++ 内存模块.h
@@ -1,27 +1,18 @@
-// 写入示例
-//写内存f64(TARGET_ADDRESS, 2.71828182845904);
-//printf("写入 double 数据: 2.71828182845904\n");
-//double_value = 读内存f64(TARGET_ADDRESS);
-//printf("读取 double 数据: %f\n", double_value);
-
-// 读取示例
-//int32_t int32_value = 读内存i32(TARGET_ADDRESS);
-//printf("读取 int32_t 数据: %d\n", int32_value);
-
 #ifndef MEMORY_READ_H
 #define MEMORY_READ_H
 
 #include <mach/mach.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 
-int initialize_task_port(pid_t pid);//获取任务端口
-void cleanup_task_port();			//释放任务端口
+int initialize_memory_access(pid_t pid, mach_vm_address_t address, size_t size);
+void cleanup_memory_access();
 
 // 读取函数
 int32_t 读内存i32(mach_vm_address_t address);
 int64_t 读内存i64(mach_vm_address_t address);
-float 	读内存f32(mach_vm_address_t address);
-double 	读内存f64(mach_vm_address_t address);
+float   读内存f32(mach_vm_address_t address);
+double  读内存f64(mach_vm_address_t address);
 
 // 写入函数
 void 写内存i32(mach_vm_address_t address, int32_t value);
