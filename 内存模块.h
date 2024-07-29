dcb535ff29af65100a43e82b77639a5441f69c2f
11265 <xdllll@163.com>
2024-07-30 02:01:06 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -41,7 +41,7 @@ int 写内存i64(vm_address_t address, int64_t value);
 int 写内存f32(vm_address_t address, float value);
 int 写内存f64(vm_address_t address, double value);
 
-static MemoryRegion* get_or_create_page(vm_address_t address);
+MemoryRegion* get_or_create_page(vm_address_t address);
 void* 处理内存请求(void* arg);
 
 #endif // MEMORY_MODULE_H
