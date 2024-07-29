65728dfc8ce2d896a8aa6a7896dd68345058656a
11265 <xdllll@163.com>
2024-07-30 01:20:33 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -11,6 +11,7 @@
 #define MIN_CACHED_REGIONS 5
 #define MAX_CACHED_REGIONS 100
 #define MAX_PENDING_REQUESTS 100
+#define NUM_THREADS 4
 
 typedef struct {
     vm_address_t base_address;
@@ -18,7 +19,7 @@ typedef struct {
     vm_size_t mapped_size;
     uint32_t access_count;
     time_t last_access;
-} MemoryRegion;
+} __attribute__((aligned(4))) MemoryRegion;
 
 typedef struct {
     vm_address_t address;
@@ -27,8 +28,8 @@ typedef struct {
     pthread_mutex_t *mutex;
     int *completed;
     int is_write;
-    char write_value[sizeof(double)];  // 足够大的缓冲区来存储任何类型
-} MemoryRequest;
+    char write_value[8];
+} __attribute__((aligned(4))) MemoryRequest;
 
 int initialize_memory_module(pid_t pid);
 void cleanup_memory_module(void);
@@ -36,25 +37,13 @@ void cleanup_memory_module(void);
 // 同步读取函数
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
-float 	读内存float(vm_address_t address);
-double 	读内存double(vm_address_t address);
-
-// 异步读取函数
-int32_t 异步读内存i32(vm_address_t address);
-int64_t 异步读内存i64(vm_address_t address);
-float 	异步读内存float(vm_address_t address);
-double 	异步读内存double(vm_address_t address);
+float   读内存f32(vm_address_t address);
+double  读内存f64(vm_address_t address);
 
 // 同步写入函数
 int 写内存i32(vm_address_t address, int32_t value);
 int 写内存i64(vm_address_t address, int64_t value);
-int 写内存float(vm_address_t address, float value);
-int 写内存double(vm_address_t address, double value);
-
-// 异步写入函数
-int 异步写内存i32(vm_address_t address, int32_t value);
-int 异步写内存i64(vm_address_t address, int64_t value);
-int 异步写内存float(vm_address_t address, float value);
-int 异步写内存double(vm_address_t address, double value);
+int 写内存f32(vm_address_t address, float value);
+int 写内存f64(vm_address_t address, double value);
 
 #endif // MEMORY_MODULE_H
