1caf7011761b6ea0235dd8601fe9ae5113573055
11265 <xdllll@163.com>
2024-07-29 20:10:15 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -1,95 +1,8 @@
 #include "内存模块.h"
+#include "查找进程.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <mach/mach.h>
-#include <sys/sysctl.h>
-#include <errno.h>
-#include <stdbool.h>
 
 #define TARGET_PROCESS_NAME "pvz"
-#define PROC_PIDPATHINFO_MAXSIZE  (4 * MAXPATHLEN)
-
-typedef struct kinfo_proc kinfo_proc;
-
-static int get_proc_list(kinfo_proc **procList, size_t *procCount) {
-    int                 err;
-    kinfo_proc *        result;
-    bool                done;
-    static const int    name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
-    size_t              length;
-
-    *procCount = 0;
-
-    result = NULL;
-    done = false;
-    do {
-        length = 0;
-        err = sysctl((int *)name, (sizeof(name) / sizeof(*name)) - 1,
-                     NULL, &length,
-                     NULL, 0);
-        if (err == -1) {
-            err = errno;
-        }
-
-        if (err == 0) {
-            result = malloc(length);
-            if (result == NULL) {
-                err = ENOMEM;
-            }
-        }
-
-        if (err == 0) {
-            err = sysctl((int *)name, (sizeof(name) / sizeof(*name)) - 1,
-                         result, &length,
-                         NULL, 0);
-            if (err == -1) {
-                err = errno;
-            }
-            if (err == 0) {
-                done = true;
-            } else if (err == ENOMEM) {
-                free(result);
-                result = NULL;
-                err = 0;
-            }
-        }
-    } while (err == 0 && !done);
-
-    if (err != 0 && result != NULL) {
-        free(result);
-        result = NULL;
-    }
-
-    if (err == 0) {
-        *procList = result;
-        *procCount = length / sizeof(kinfo_proc);
-    }
-
-    return err;
-}
-
-pid_t get_pid_by_name(const char *process_name) {
-    kinfo_proc *procList = NULL;
-    size_t procCount = 0;
-    int err = get_proc_list(&procList, &procCount);
-    
-    if (err != 0) {
-        fprintf(stderr, "无法获取进程列表: %d\n", err);
-        return -1;
-    }
-
-    pid_t target_pid = -1;
-    for (size_t i = 0; i < procCount; i++) {
-        if (strcmp(procList[i].kp_proc.p_comm, process_name) == 0) {
-            target_pid = procList[i].kp_proc.p_pid;
-            break;
-        }
-    }
-
-    free(procList);
-    return target_pid;
-}
 
 int c_main(void) {
     pid_t target_pid = get_pid_by_name(TARGET_PROCESS_NAME);
@@ -99,7 +12,6 @@ int c_main(void) {
     }
     
     printf("找到进程 %s，PID: %d\n", TARGET_PROCESS_NAME, target_pid);
-    printf("开始初始化内存访问...\n");
 
     int result = initialize_memory_module(target_pid);
     if (result != 0) {
