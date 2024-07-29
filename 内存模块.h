0b79f77f822b46559ca1482480abcf395665847d
11265 <xdllll@163.com>
2024-07-29 16:58:57 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -4,12 +4,12 @@
 #include <sys/types.h>
 #include <mach/mach_types.h>
 
-int initialize_memory_access(pid_t pid, mach_vm_address_t address, size_t size);
+int initialize_memory_access(pid_t pid, vm_address_t address, size_t size);
 void cleanup_memory_access();
 
-int32_t 读内存i32(mach_vm_address_t address);
-int64_t 读内存i64(mach_vm_address_t address);
-float   读内存f32(mach_vm_address_t address);
-double  读内存f64(mach_vm_address_t address);
+int32_t 读内存i32(vm_address_t address);
+int64_t 读内存i64(vm_address_t address);
+float   读内存f32(vm_address_t address);
+double  读内存f64(vm_address_t address);
 
 #endif
