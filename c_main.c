3c5be2b0a136d03c8e03aff011c28619c0343fd1
11265 <xdllll@163.com>
2024-07-29 19:56:06 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -1,14 +1,41 @@
 #include "内存模块.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <mach/mach.h>
+#include <libproc.h>
 
-#define TARGET_PID 23569
+#define MAX_PROCESS_NAME_LENGTH 1024
+#define TARGET_PROCESS_NAME "pvz"
+
+// 通过进程名称获取PID
+pid_t get_pid_by_name(const char *process_name) {
+    pid_t pids[2048];
+    int bytes = proc_listpids(PROC_ALL_PIDS, 0, pids, sizeof(pids));
+    int n_proc = bytes / sizeof(pids[0]);
+    
+    for (int i = 0; i < n_proc; i++) {
+        char name[MAX_PROCESS_NAME_LENGTH];
+        proc_name(pids[i], name, sizeof(name));
+        if (strcmp(name, process_name) == 0) {
+            return pids[i];
+        }
+    }
+    return -1;
+}
 
 int c_main(void) {
+    pid_t target_pid = get_pid_by_name(TARGET_PROCESS_NAME);
+    if (target_pid == -1) 
+    {
+        fprintf(stderr, "未找到进程：%s\n", TARGET_PROCESS_NAME);
+        return -1;
+    }
+    
+    printf("找到进程 %s，PID: %d\n", TARGET_PROCESS_NAME, target_pid);
     printf("开始初始化内存访问...\n");
 
-    int result = initialize_memory_module(TARGET_PID);
+    int result = initialize_memory_module(target_pid);
     if (result != 0) {
         fprintf(stderr, "无法初始化内存模块，错误代码：%d\n", result);
         return -1;
