8b0c47870df9320a6d5d95552e5ba1c6d7aefb73
11265 <xdllll@163.com>
2024-07-29 15:28:51 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -6,7 +6,7 @@
 
 int initialize_task_port(pid_t pid);                            // 初始化任务端口的函数声明
 void cleanup_task_port();                                       // 清理任务端口的函数声明
-int read_int32(mach_vm_address_t address, int32_t *value);      // 读取32位整数的函数声明
-int 读i64(mach_vm_address_t address, int64_t *value);      // 读取64位整数的函数声明
+int 读内存i32(mach_vm_address_t address, int32_t *value);      // 读取32位整数的函数声明
+int 读内存i64(mach_vm_address_t address, int64_t *value);      // 读取64位整数的函数声明
 
 #endif                                                          // 结束头文件保护
