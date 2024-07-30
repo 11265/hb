f787b709bc8728a2d4ffc9e0bbb337aa2e727ce1
11265 <xdllll@163.com>
2024-07-30 12:48:29 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -15,31 +15,35 @@ typedef struct {
     size_t mapped_size;
     uint32_t access_count;
     time_t last_access;
-    int is_large_mapping;  // New field to indicate if this is a large memory mapping
+    int is_large_mapping;
 } MemoryRegion;
 
 typedef struct {
-    int operation;  // 0 for read, 1 for write
+    int operation;
     vm_address_t address;
     void* buffer;
     size_t size;
     void* result;
 } MemoryRequest;
 
-int 初始化内存模块(pid_t pid);
+int  初始化内存模块(pid_t pid);
 void 关闭内存模块();
 
 void* 读任意地址(vm_address_t address, size_t size);
-int 写任意地址(vm_address_t address, const void* data, size_t size);
+int   写任意地址(vm_address_t address, const void* data, size_t size);
 
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
-float 读内存f32(vm_address_t address);
-double 读内存f64(vm_address_t address);
+float   读内存f32(vm_address_t address);
+double  读内存f64(vm_address_t address);
 
 int 写内存i32(vm_address_t address, int32_t value);
 int 写内存i64(vm_address_t address, int64_t value);
 int 写内存f32(vm_address_t address, float value);
 int 写内存f64(vm_address_t address, double value);
+
+// 新增函数声明
+void* 内存分配(size_t size);
+void  内存释放(void* ptr);
 
 #endif // MEMORY_MODULE_H
