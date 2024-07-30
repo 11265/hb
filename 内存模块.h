2d9d9b70cf0bbb4c6f31a275f2a478c95421ff9c
11265 <xdllll@163.com>
2024-07-30 13:12:18 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -8,6 +8,8 @@
 #define INITIAL_CACHED_REGIONS 100
 #define NUM_THREADS 4
 #define MAX_PENDING_REQUESTS 1000
+#define MEMORY_POOL_SIZE 1024 * 1024  // 1MB 内存池
+#define SMALL_ALLOCATION_THRESHOLD 256  // 小于此值的分配使用内存池
 
 typedef struct {
     vm_address_t base_address;
@@ -25,16 +27,26 @@ typedef struct {
     void* result;  // 用于存储操作结果
 } MemoryRequest;
 
-int 初始化内存模块(pid_t pid);
+typedef struct MemoryChunk {
+    struct MemoryChunk* next;
+    size_t size;
+} MemoryChunk;
+
+typedef struct {
+    void* pool;
+    MemoryChunk* free_list;
+} MemoryPool;
+
+int  初始化内存模块(pid_t pid);
 void 关闭内存模块();
 
 void* 读任意地址(vm_address_t address, size_t size);
-int 写任意地址(vm_address_t address, const void* data, size_t size);
+int   写任意地址(vm_address_t address, const void* data, size_t size);
 
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
-float 读内存f32(vm_address_t address);
-double 读内存f64(vm_address_t address);
+float   读内存f32(vm_address_t address);
+double  读内存f64(vm_address_t address);
 
 int 写内存i32(vm_address_t address, int32_t value);
 int 写内存i64(vm_address_t address, int64_t value);
@@ -43,5 +55,10 @@ int 写内存f64(vm_address_t address, double value);
 
 MemoryRegion* get_or_create_page(vm_address_t address);
 void* 处理内存请求(void* arg);
+
+void  初始化内存池(MemoryPool* pool);
+void* 内存池分配(MemoryPool* pool, size_t size);
+void  内存池释放(MemoryPool* pool, void* ptr);
+void  销毁内存池(MemoryPool* pool);
 
 #endif // MEMORY_MODULE_H
