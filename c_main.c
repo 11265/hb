08df15113d555da6998808a5f453a696a9479e26
11265 <xdllll@163.com>
2024-07-30 19:23:28 +0800

    更新 c_main.c

--- c_main.c
+++ c_main.c
@@ -23,16 +23,29 @@ int c_main(void) {
 
     vm_address_t test_address = 0x102F83E68; // 示例地址，请根据实际情况修改
 
+    // 检查内存保护
+    vm_prot_t protection;
+    boolean_t shared;
+    vm_size_t size = sizeof(int32_t);
+    kern_return_t kr = vm_region(target_task, &test_address, &size, VM_REGION_BASIC_INFO, (vm_region_info_t)&protection, &shared);
+    if (kr != KERN_SUCCESS) {
+        printf("无法获取内存区域信息: %s\n", mach_error_string(kr));
+        关闭内存模块();
+        return 1;
+    }
+    printf("内存保护: %d\n", protection);
+
     // 读取初始 int32 值
     int32_t initial_value = 读内存i32(test_address);
     printf("初始 int32 值: %d\n", initial_value);
 
     // 写入新的 int32 值
     int32_t new_value = 42; // 您可以修改这个值
-    if (写内存i32(test_address, new_value) == 0) {
+    int write_result = 写内存i32(test_address, new_value);
+    if (write_result == 0) {
         printf("成功写入新的 int32 值: %d\n", new_value);
     } else {
-        printf("写入新的 int32 值失败\n");
+        printf("写入新的 int32 值失败, 错误码: %d\n", write_result);
     }
 
     // 再次读取 int32 值
