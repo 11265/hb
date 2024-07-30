cc0d91ffef4d0260975be3dc31db5a50922b0c90
11265 <xdllll@163.com>
2024-07-30 13:05:17 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -2,8 +2,8 @@
 #define MEMORY_MODULE_H
 
 #include <mach/mach.h>
-#include <sys/types.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define INITIAL_CACHED_REGIONS 100
 #define NUM_THREADS 4
@@ -15,36 +15,33 @@ typedef struct {
     size_t mapped_size;
     uint32_t access_count;
     time_t last_access;
-    int is_large_mapping;
 } MemoryRegion;
 
 typedef struct {
-    int operation;
     vm_address_t address;
-    void* buffer;
     size_t size;
-    void* result;
+    void* buffer;
+    int operation; // 0 for read, 1 for write
+    void* result;  // 用于存储操作结果
 } MemoryRequest;
 
-int  初始化内存模块(pid_t pid);
+int 初始化内存模块(pid_t pid);
 void 关闭内存模块();
 
-void* 内存分配(size_t size);
-void  内存释放(void* ptr);
-
-void* 读任意地址(vm_address_t address, void* buffer, size_t size);
-int   写任意地址(vm_address_t address, const void* data, size_t size);
+void* 读任意地址(vm_address_t address, size_t size);
+int 写任意地址(vm_address_t address, const void* data, size_t size);
 
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
-float   读内存f32(vm_address_t address);
-double  读内存f64(vm_address_t address);
+float 读内存f32(vm_address_t address);
+double 读内存f64(vm_address_t address);
 
 int 写内存i32(vm_address_t address, int32_t value);
 int 写内存i64(vm_address_t address, int64_t value);
 int 写内存f32(vm_address_t address, float value);
 int 写内存f64(vm_address_t address, double value);
 
-
+MemoryRegion* get_or_create_page(vm_address_t address);
+void* 处理内存请求(void* arg);
 
 #endif // MEMORY_MODULE_H
