96bff340dea39ebbeb44c6447381015b03d67af0
11265 <xdllll@163.com>
2024-07-30 16:07:55 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -4,44 +4,57 @@
 #include <mach/mach.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <pthread.h>
 
-#define INITIAL_CACHED_REGIONS 100
-#define NUM_THREADS 4
-#define MAX_PENDING_REQUESTS 1000
+#define REGION_SIZE (1024 * 1024)  // 1MB
+#define INITIAL_CACHED_REGIONS 10
+#define MIN_CACHED_REGIONS 5
+#define MAX_CACHED_REGIONS 100
+#define MAX_PENDING_REQUESTS 100
 
 typedef struct {
     vm_address_t base_address;
-    void* mapped_memory;
-    size_t mapped_size;
+    void *mapped_memory;
+    vm_size_t mapped_size;
     uint32_t access_count;
     time_t last_access;
 } MemoryRegion;
 
 typedef struct {
     vm_address_t address;
-    size_t size;
-    void* buffer;
-    int operation; // 0 for read, 1 for write
-    void* result;  // 用于存储操作结果
+    void *result;
+    pthread_cond_t *cond;
+    pthread_mutex_t *mutex;
+    int *completed;
+    int is_write;
+    char write_value[sizeof(double)];  // 足够大的缓冲区来存储任何类型
 } MemoryRequest;
 
-int 初始化内存模块(pid_t pid);
-void 关闭内存模块();
+int initialize_memory_module(pid_t pid);
+void cleanup_memory_module(void);
 
-void* 读任意地址(vm_address_t address, size_t size);
-int 写任意地址(vm_address_t address, const void* data, size_t size);
-
+// 同步读取函数
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
-float 读内存f32(vm_address_t address);
-double 读内存f64(vm_address_t address);
+float 	读内存float(vm_address_t address);
+double 	读内存double(vm_address_t address);
 
+// 异步读取函数
+int32_t 异步读内存i32(vm_address_t address);
+int64_t 异步读内存i64(vm_address_t address);
+float 	异步读内存float(vm_address_t address);
+double 	异步读内存double(vm_address_t address);
+
+// 同步写入函数
 int 写内存i32(vm_address_t address, int32_t value);
 int 写内存i64(vm_address_t address, int64_t value);
-int 写内存f32(vm_address_t address, float value);
-int 写内存f64(vm_address_t address, double value);
+int 写内存float(vm_address_t address, float value);
+int 写内存double(vm_address_t address, double value);
 
-MemoryRegion* get_or_create_page(vm_address_t address);
-void* 处理内存请求(void* arg);
+// 异步写入函数
+int 异步写内存i32(vm_address_t address, int32_t value);
+int 异步写内存i64(vm_address_t address, int64_t value);
+int 异步写内存float(vm_address_t address, float value);
+int 异步写内存double(vm_address_t address, double value);
 
 #endif // MEMORY_MODULE_H
