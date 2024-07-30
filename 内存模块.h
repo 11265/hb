d3153b64e6eabd64aa4d81b09f58ccd5ac836ca4
11265 <xdllll@163.com>
2024-07-30 11:42:56 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -3,17 +3,15 @@
 #ifndef MEMORY_MODULE_H
 #define MEMORY_MODULE_H
 
-#include <mach/mach.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <mach/mach.h>
 
-#define INITIAL_CACHED_REGIONS 100
 #define NUM_THREADS 4
-#define MAX_PENDING_REQUESTS 1000
-
+#define MAX_PENDING_REQUESTS 100
+#define INITIAL_CACHED_REGIONS 100
 #define MEMORY_POOL_BLOCK_SIZE 4096
-#define MEMORY_POOL_BLOCK_COUNT 1024
+#define MEMORY_POOL_BLOCK_COUNT 100
 
 typedef struct {
     vm_address_t base_address;
@@ -24,14 +22,6 @@ typedef struct {
 } MemoryRegion;
 
 typedef struct {
-    vm_address_t address;
-    size_t size;
-    void* buffer;
-    int operation; // 0 for read, 1 for write
-    void* result;  // 用于存储操作结果
-} MemoryRequest;
-
-typedef struct MemoryBlock {
     void* memory;
     struct MemoryBlock* next;
 } MemoryBlock;
@@ -41,28 +31,25 @@ typedef struct {
     pthread_mutex_t mutex;
 } MemoryPool;
 
+typedef struct {
+    vm_address_t address;
+    void* buffer;
+    size_t size;
+    int operation;  // 0 for read, 1 for write
+    void* result;
+} MemoryRequest;
+
 int   初始化内存模块(pid_t pid);
 void  关闭内存模块();
-
-void*   读任意地址(vm_address_t address, size_t size);
-int     写任意地址(vm_address_t address, const void* data, size_t size);
-
+void* 读任意地址(vm_address_t address, void* buffer, size_t size);
+int   写任意地址(vm_address_t address, const void* data, size_t size);
 int32_t 读内存i32(vm_address_t address);
 int64_t 读内存i64(vm_address_t address);
 float   读内存f32(vm_address_t address);
 double  读内存f64(vm_address_t address);
-
-int 写内存i32(vm_address_t address, int32_t value);
-int 写内存i64(vm_address_t address, int64_t value);
-int 写内存f32(vm_address_t address, float value);
-int 写内存f64(vm_address_t address, double value);
-
-MemoryRegion* get_or_create_page(vm_address_t address);
-void* 处理内存请求(void* arg);
-
-void  初始化内存池();
-void* 内存池分配(size_t size);
-void  内存池释放(void* ptr);
-void  清理内存池();
+int     写内存i32(vm_address_t address, int32_t value);
+int     写内存i64(vm_address_t address, int64_t value);
+int     写内存f32(vm_address_t address, float value);
+int     写内存f64(vm_address_t address, double value);
 
 #endif // MEMORY_MODULE_H
