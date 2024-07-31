7f7e7bdef86c24a525724b0f035f4adea1b9790e
11265 <xdllll@163.com>
2024-07-31 18:58:36 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -1,63 +1,27 @@
 #ifndef MEMORY_MODULE_H
 #define MEMORY_MODULE_H
 
-#include <mach/mach.h>
+#include <sys/types.h>
 #include <stdint.h>
 #include <unistd.h>
-#include <pthread.h>
-
-#define INITIAL_CACHED_REGIONS 100
-#define NUM_THREADS 4
-#define MAX_PENDING_REQUESTS 1000
-#define MEMORY_POOL_SIZE (10 * 1024 * 1024)  // 10MB 内存池
-#define SMALL_ALLOCATION_THRESHOLD 256  // 小于此值的分配使用内存池
-
-typedef struct MemoryChunk {
-    struct MemoryChunk* next;
-    size_t size;
-} MemoryChunk;
+#include <time.h>
 
 typedef struct {
-    void* pool;
-    MemoryChunk* free_list;
-} MemoryPool;
-
-extern MemoryPool memory_pool;
-
-typedef struct {
-    vm_address_t base_address;
-    void* mapped_memory;
+    void* mapped_address;
     size_t mapped_size;
-    uint32_t access_count;
-    time_t last_access;
-} MemoryRegion;
+    uintptr_t target_address;
+    struct timespec last_update;
+    int update_interval_ms;
+} MappedMemory;
 
-typedef struct {
-    void* data;
-    int from_pool;
-} MemoryReadResult;
-
-typedef struct {
-    vm_address_t address;
-    size_t size;
-    void* buffer;
-    int operation;
-    void* result;
-} MemoryRequest;
-
-void  初始化内存池(MemoryPool* pool);
-void* 内存池分配(MemoryPool* pool, size_t size);
-void  内存池释放(MemoryPool* pool, void* ptr);
-void  销毁内存池(MemoryPool* pool);
-
-MemoryReadResult 读任意地址(vm_address_t address, size_t size);
-
-int32_t 读内存i32(vm_address_t address);
-int64_t 读内存i64(vm_address_t address);
-float   读内存f32(vm_address_t address);
-double  读内存f64(vm_address_t address);
-
-int  初始化内存模块(pid_t pid);
-void 关闭内存模块();
+int     初始化内存模块(pid_t pid);
+void     关闭内存模块();
+MappedMemory* 创建内存映射(uintptr_t target_address, size_t size, int update_interval_ms);
+void    释放内存映射(MappedMemory* mapped_memory);
+int     更新内存映射(MappedMemory* mapped_memory);
+int32_t 读内存i32(MappedMemory* mapped_memory, uintptr_t offset);
+int64_t 读内存i64(MappedMemory* mapped_memory, uintptr_t offset);
+float   读内存f32(MappedMemory* mapped_memory, uintptr_t offset);
+double  读内存f64(MappedMemory* mapped_memory, uintptr_t offset);
 
 #endif // MEMORY_MODULE_H
