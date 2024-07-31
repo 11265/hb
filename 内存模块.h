33baa8e00fcc200a6c71460d9db4457b500f7fd1
11265 <xdllll@163.com>
2024-07-31 19:14:42 +0800

    更新 内存模块.h

--- 内存模块.h
+++ 内存模块.h
@@ -9,16 +9,15 @@
 typedef struct {
     void* mapped_address;
     size_t mapped_size;
-    uintptr_t target_address;
-    struct timespec last_update;
-    int update_interval_ms;
+    int mem_fd;
 } MappedMemory;
 
+MappedMemory* 创建内存映射(uintptr_t target_address, size_t size);
+
 int     初始化内存模块(pid_t pid);
-void     关闭内存模块();
-MappedMemory* 创建内存映射(uintptr_t target_address, size_t size, int update_interval_ms);
+void    关闭内存模块();
+
 void    释放内存映射(MappedMemory* mapped_memory);
-int     更新内存映射(MappedMemory* mapped_memory);
 int32_t 读内存i32(MappedMemory* mapped_memory, uintptr_t offset);
 int64_t 读内存i64(MappedMemory* mapped_memory, uintptr_t offset);
 float   读内存f32(MappedMemory* mapped_memory, uintptr_t offset);
